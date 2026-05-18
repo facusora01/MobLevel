286baa9237d954bafa48fc9e4089ca9da603a434
facusora01 <fsorasio@fi.uba.ar>
2026-05-18 01:53:36 -0300

    arreglos

--- src/main/java/com/moblevel/MobLevel.java
+++ src/main/java/com/moblevel/MobLevel.java
@@ -3,7 +3,6 @@ package com.moblevel;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -14,25 +13,13 @@ import net.minecraftforge.fml.config.ModConfig;
 public class MobLevel {
     public static final String MODID = "moblevel";
     public static final Logger LOGGER = LogUtils.getLogger();
-    private static boolean registered = false;
 
     public MobLevel() {
-        if (!registered) {
-            try {
-                java.lang.reflect.Field f = ModLoadingContext.class.getDeclaredField("context");
-                f.setAccessible(true);
-                ModLoadingContext ctx = (ModLoadingContext) f.get(null);
-                java.lang.reflect.Method m = ModLoadingContext.class.getDeclaredMethod("getModEventBus");
-                m.setAccessible(true);
-                IEventBus modEventBus = (IEventBus) m.invoke(ctx);
-                ModItems.register(modEventBus);
-                ModCreativeModeTabs.register(modEventBus);
-                registered = true;
-            } catch (Exception e) {
-                LOGGER.error("Failed to register items via reflection", e);
-            }
+        try {
+            ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        } catch (Exception e) {
+            LOGGER.error("Failed to register config", e);
         }
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
---
+++ src/main/java/com/moblevel/ModSetup.java
@@ -0,0 +1,4 @@
+package com.moblevel;
+
+public class ModSetup {
+}
